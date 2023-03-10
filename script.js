696cc6b142438615b108bb1a0882fb83e3a43e0b
ThePyGuy <104279097+ThePyGuyYe@users.noreply.github.com>
2023-03-10 17:17:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-function sendEmail() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var subject = document.getElementById("subject").value;
-    var message = document.getElementById("message").value;
-  
-    if (name == "" || email == "" || subject == "" || message == "") {
-      alert("Please fill in all fields");
-    } else {
-      var link = "mailto:thepyguyteam@gmail.com"
-              
+// Get the file names from the server using AJAX
+function getFileNames() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      displayFileNames(this.responseText);
+    }
+  };
+  xhttp.open("GET", "getFiles.php", true);
+  xhttp.send();
+}
+
+// Display the file names in a list on the webpage
+function displayFileNames(fileNames) {
+  var fileList = document.getElementById("fileList");
+  var files = fileNames.split("\n");
+  for (var i = 0; i < files.length; i++) {
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    a.setAttribute("href", files[i]);
+    a.innerHTML = files[i];
+    li.appendChild(a);
+    fileList.appendChild(li);
+  }
+}
+
+// Call the getFileNames function when the webpage loads
+window.onload = function() {
+  getFileNames();
+};
